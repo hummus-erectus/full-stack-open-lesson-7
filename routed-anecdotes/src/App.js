e8ab31b602bb0544152cf19897da987a3f780198
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-26 14:38:27 +0900

    Complete 7.5

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -82,6 +82,12 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -98,7 +104,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='button' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
